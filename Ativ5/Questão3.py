152956d685e565c7a3e0d8b5449bc883abe7149b
ReisSalv07 <gabrielreis200509@gmail.com>
2026-03-27 10:58:51 -0300

    Update Questão3.py

--- Ativ5/Questão3.py
+++ Ativ5/Questão3.py
@@ -3,7 +3,6 @@ from estruturas_dados import Fila
 fila_pedidos = Fila()
 print(f"Fila de pedidos inicial: {fila_pedidos}")
 
-print("\n--- Inserindo pedidos ---")
 fila_pedidos.enfileirar("Pedido 1 (Pizza Calabresa)")
 print(f"Após enfileirar 'Pedido 1 (Pizza Calabresa)': {fila_pedidos}")
 fila_pedidos.enfileirar("Pedido 2 (Lasanha)")
@@ -11,7 +10,6 @@ print(f"Após enfileirar 'Pedido 2 (Lasanha)': {fila_pedidos}")
 fila_pedidos.enfileirar("Pedido 3 (Refrigerante)")
 print(f"Após enfileirar 'Pedido 3 (Refrigerante)': {fila_pedidos}")
 
-print("\n--- Verificando situações da fila ---")
 print(f"Primeiro pedido na fila: {fila_pedidos.primeiro()}")
 print(f"Último pedido na fila: {fila_pedidos.ultimo()}")
 print(f"Quantidade de pedidos na fila: {fila_pedidos.tamanho()}")
