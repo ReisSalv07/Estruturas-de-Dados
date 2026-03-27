19f66d3c074257067faacc726d84abd7d4fba569
ReisSalv07 <gabrielreis200509@gmail.com>
2026-03-27 10:54:39 -0300

    Update Questão2.py

--- Ativ5/Questão2.py
+++ Ativ5/Questão2.py
@@ -15,20 +15,3 @@ print("\n--- Verificando situações da fila ---")
 print(f"Primeiro cliente na fila: {fila_banco.primeiro()}")
 print(f"Último cliente na fila: {fila_banco.ultimo()}")
 print(f"Quantidade de clientes na fila: {fila_banco.tamanho()}")
-
-print("\n--- Simulando atendimento ---")
-cliente_atendido = fila_banco.desenfileirar()
-print(f"Cliente atendido: {cliente_atendido}. Fila atual: {fila_banco}")
-print(f"Primeiro cliente na fila agora: {fila_banco.primeiro()}")
-print(f"Quantidade de clientes na fila agora: {fila_banco.tamanho()}")
-
-cliente_atendido = fila_banco.desenfileirar()
-print(f"Cliente atendido: {cliente_atendido}. Fila atual: {fila_banco}")
-print(f"Quantidade de clientes na fila agora: {fila_banco.tamanho()}")
-
-print("\n--- Verificando fila vazia ---")
-print(f"A fila está vazia? {fila_banco.esta_vazia()}")
-fila_banco.desenfileirar() # Tentando desenfileirar de uma fila com um item
-fila_banco.desenfileirar() # Tentando desenfileirar de uma fila vazia
-print(f"Fila após tentar desenfileirar mais: {fila_banco}")
-print(f"A fila está vazia? {fila_banco.esta_vazia()}")
